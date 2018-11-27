dac4f630cd83d7c25e5d689e599233ad95531750
Jongsoo Jeong <jsjeong@coxlab.kr>
2018-11-27 16:31:11 +0900

    Updated.

--- Basic/main.cpp
+++ Basic/main.cpp
@@ -8,6 +8,13 @@ Timer timerHello;
 void setup() {
   Serial.begin(115200);
   Serial.printf("\n*** [ST Nucleo-L073RZ] Basic Functions ***\n");
+  Serial.listen();
+  Serial.onReceive([](SerialPort &p) {
+    while (p.available() > 0) {
+      char c = p.read();
+      p.write(c);
+    }
+  });
 
   pinMode(LD2, OUTPUT);
 
@@ -21,9 +28,16 @@ void setup() {
   timerHello.startPeriodic(1000);
 
   attachInterrupt(
-    PC13,
+    BUTTON,
     []() {
       printf("Button pressed!\n");
+      if (Serial.isListening()) {
+        Serial.stopListening();
+        printf("* Stop Serial listening.\n");
+      } else {
+        printf("* Listen on Serial.\n");
+        Serial.listen();
+      }
     },
     FALLING
   );
